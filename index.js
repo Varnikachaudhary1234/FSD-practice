c26c458b63bb4bebba45ffe9e341e410e185c8fa
Varnikachaudhary1234 <varnikachaudhary2004@gmail.com>
2025-09-30 15:36:26 +0530

    bookticket

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-//console.log(a);
-//var a=30;
-//console.log(a);
-//{
-//    var a=50;
-//    console.log(a);
-//
-//}
-//console.log(a);
-//explicit type conversion
-
-//let strToNum=Number("hello");
-//console.log(  strToNum+" " +typeof(strToNum));
-
-//let numTostr= String(1234);
-//console.log(numTostr+" "+typeof(numTostr));
-
-//let numToBool = Boolean(" ");
-//console.log(numToBool);//no value then always give false
-
-
-//console.log(numToBool);
-//for loop
-let row=4;
-for(let i=0;i<row;i++){
-    let line=""
-    for(let j=0;j<2*row-1;j++){
-        if(i+j>row && j-i<row-1){
-            line+="*";
-        }
-        else{
-            line+=" ";
-        }
-    }
-    console.log();
+const printMessage = (msg)=>{
+    console.log(msg);
 }
-for(i in [1,2,3,4,5]){
-    console.log();
-}
+const bookTicket = ()=>{
+    printMessage("Welcome!");
+    setTimeout(()=>{
+        printMessage("login succesfully");
+        setTimeout(()=>{
+            printMessage("seat has been selected successfully.");
+            setTimeout(()=>{
+                printMessage("your seat had been confirmed .proceed for payment.");
+                setTimeout(()=>{
+                    printMessage("payment has been done");
+                    setTimeout(()=>{
+                        printMessage("ticket has been issued");
+                        printMessage("Thanku for visiting our site.");
+                    },3000)
+                },4000)
 
+            },3000)
+        },2000)
+
+    },3000)
+}
